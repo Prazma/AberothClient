73c412a718afe52e743ab3744ad068743cba532d
Terminus-- <collin.pritchard@yahoo.com>
2018-03-22 11:24:38 -0400

    Create jsfocus.js

---
+++ src/jsfocus.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+   initializeClickListener();
+});
+
+function initializeClickListener(){
+   var canvas = document.getElementById("screen");
+   canvas.addEventListener("mousedown", getFocusOnMouseDown, false);
+}
+
+function getFocusOnMouseDown(event){
+   window.focus();
+   document.getElementById("screen").focus();
